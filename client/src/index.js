c6a02a854a5101dfe9800748c88adab84c622f4e
Adam <adam.bardsley@gmail.com>
2019-03-07 16:44:24 +0000

    Trying to meet all webAPK reqs.

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,40 @@ const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log("beforeinstall")
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register({'serviceWorkerInDev': true});
+
+let btnAdd = document.getElementById('install')
+btnAdd.addEventListener('click', (e) => {
+    console.log("attempt the install!")
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+
+  window.addEventListener('appinstalled', (evt) => {
+    console.logEvent('a2hs', 'installed');
+  });
